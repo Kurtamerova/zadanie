59cece19d9cb99c479e1cc6f29aa91682757e6d8
Kurtamerova <kk_lla@mail.ru>
2022-05-19 14:54:03 +0500

    Add file

---
+++ zadanie/main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks': tasks})
+
+
+def about(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'main/about.html', context)
